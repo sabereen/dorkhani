e35c6109da537e02749a7eaa6b8352258916f143
sabereen <mqtstbh@gmail.com>
2025-03-07 16:08:33 +0330

    fix: fix prisma production error

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,4 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
+
+// @ts-expect-error تایپ اسکریپت ندارد
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
+const { PrismaClient: RealPrismaClient } = require('@prisma/client')
 
 // expose a singleton
-export const db = new PrismaClient()
+export const db: PrismaClient = new RealPrismaClient()

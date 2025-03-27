c029ee131da5850a97d013f978fbc84db8b68c91
sabereen <mqtstbh@gmail.com>
2025-03-27 04:59:39 +0330

    feat: multiple ayah select

---
+++ src/routes/api/khatm/pickNext/+server.ts
@@ -0,0 +1,54 @@
+import { db } from '$lib/server/db'
+import { error, json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { COUNT_OF_AYAHS } from '@ghoran/metadata/constants'
+import { importText } from '@ghoran/text'
+import translation from '@ghoran/translation/json/fa/tanzil-ansarian.json'
+import type { Khatm } from '@prisma/client'
+
+export type SelectedAyah = {
+	index: number
+	text: string
+	translation: string
+}
+
+export type PickAyahResult = {
+	khatm: Khatm
+	ayat: SelectedAyah[]
+}
+
+export const POST: RequestHandler = async (event) => {
+	const body: { khatmId: number; count: number } = await event.request.json()
+
+	if (typeof body.khatmId !== 'number' || body.count < 0 || body.count > 40) {
+		throw error(400, 'ورودی معتبر نیست')
+	}
+
+	body.count = Math.floor(body.count)
+
+	const result = await db.khatm.update({
+		where: {
+			id: body.khatmId,
+			sequential: true,
+			rangeType: 'ayah',
+			currentAyahIndex: { lt: COUNT_OF_AYAHS - body.count },
+		},
+		data: {
+			currentAyahIndex: { increment: body.count },
+		},
+	})
+
+	const text = await importText('hafs')
+
+	const ayat: SelectedAyah[] = []
+	for (let i = body.count - 1; i >= 0; i--) {
+		const ayahIndex = result.currentAyahIndex - i
+		ayat.push({
+			index: ayahIndex,
+			text: text.default[ayahIndex],
+			translation: translation[ayahIndex],
+		})
+	}
+
+	return json({ khatm: result, ayat } satisfies PickAyahResult)
+}
--- src/routes/khatm/[khatmId]/ayah-by-ayah.svelte
+++ src/routes/khatm/[khatmId]/ayah-by-ayah.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Khatm } from '@prisma/client'
 	import { slide } from 'svelte/transition'
-	import type { PickAyahResult } from '../../api/khatm/pickNextAyah/+server'
+	import type { PickAyahResult, SelectedAyah } from '../../api/khatm/pickNext/+server'
 	import { Ayah } from '@ghoran/entity'
 	import { surah_getName } from '$lib/entity/Surah'
 	import '@ghoran/text/fonts/uthmanic-hafs-v13/style.css'
@@ -14,21 +14,25 @@
 
 	const { khatm }: Props = $props()
 
-	let loading = $state(false)
-	let ayah = $state<Ayah | null>()
-	let ayahText = $state('')
-	let ayahTranslation = $state('')
+	// عدد -1 نمایش دهنده غیر فعال بودن لودینگ است
+	// برای اینکه مشخص باشد روی کدام دکمه لودینگ بخورد تعداد آیات را در لودینگ میریزیم
+	// هر دکمه‌ای که تعداد آیاتش با این متغیر یکسان بود باید لودینگ بخورد
+	let loading = $state(-1)
+	let selectedAyat = $state<SelectedAyah[]>([])
 
 	let ayahWrapper = $state<HTMLElement>()
 
-	async function pick() {
-		loading = true
+	async function pick(count = 1) {
+		if (loading !== -1) return
+
+		loading = count
 
 		try {
-			const response = await fetch('/api/khatm/pickNextAyah', {
+			const response = await fetch('/api/khatm/pickNext', {
 				method: 'POST',
 				body: JSON.stringify({
 					khatmId: khatm.id,
+					count,
 				}),
 			})
 
@@ -37,49 +41,68 @@
 			if (!response.ok) {
 				throw result
 			}
-			ayah = Ayah.get(result.ayahIndex)
-			ayahText = result.ayahText
-			ayahTranslation = result.ayahTranslation
+			selectedAyat = result.ayat
 			invalidateAll()
 			ayahWrapper?.scrollIntoView({ block: 'start', behavior: 'smooth' })
 		} catch (err) {
 			console.error(err)
 			toast('error', (err as any)?.message || String(err))
 		} finally {
-			loading = false
+			loading = -1
 		}
 	}
 </script>
 
-{#if ayah}
-	{#key ayah.index}
-		<div class="card" transition:slide|global={{ axis: 'y' }} bind:this={ayahWrapper}>
-			<div class="card-body">
-				<p class="mb-4 font-[uthmanic-hafs-v13] text-3xl leading-14">
-					{ayahText}
-					{ayah.number.toLocaleString('ar-IQ')}
-				</p>
-				<p class="text-md mb-4 opacity-80">{ayahTranslation}</p>
-				<p class="self-end text-sm">آیه {ayah.number} {surah_getName(ayah.surah)}</p>
+{#if selectedAyat.length}
+	<div bind:this={ayahWrapper}>
+		{#each selectedAyat as { index, text, translation } (index)}
+			{@const ayah = Ayah.get(index)}
+			<div class="card" transition:slide|global={{ axis: 'y' }}>
+				<div class="card-body">
+					<p class="mb-4 font-[uthmanic-hafs-v13] text-3xl leading-14">
+						{text}
+						{ayah.number.toLocaleString('ar-IQ')}
+					</p>
+					<p class="text-md mb-4 opacity-80">{translation}</p>
+					<p class="self-end text-sm">آیه {ayah.number} {surah_getName(ayah.surah)}</p>
+				</div>
 			</div>
-		</div>
-	{/key}
+		{/each}
+	</div>
 {/if}
 
 <div class="mt-5 flex flex-col text-center">
-	{#if !ayah}
-		<p class="text-lg">جهت قرائت یک آیه دیگر روی دکمه زیر کلیک کنید.</p>
+	{#if !selectedAyat.length}
+		<p class="px-4 text-lg text-balance">
+			جهت پذیرفتن قرائت یک آیه از این ختم روی دکمه زیر کلیک کنید.
+		</p>
 	{/if}
-	<div class="mt-5">
-		<button class="btn btn-primary btn-xl" onclick={pick}>
-			{#if loading}
-				<div class="loading loading-md" transition:slide={{ axis: 'x' }}></div>
-			{/if}
-			{#if ayah}
-				خواندن آیه بیشتر
-			{:else}
-				خواندن یک آیه
-			{/if}
-		</button>
+	<div class="mt-5 px-4">
+		<div class="grid grid-cols-2 gap-2">
+			{#snippet smallButton(text: string, count: number)}
+				<button class="btn btn-outline btn-sm" onclick={() => pick(count)}>
+					{#if loading === count}
+						<div class="loading loading-md" transition:slide={{ axis: 'x' }}></div>
+					{/if}
+					{text}
+				</button>
+			{/snippet}
+
+			<button class="btn btn-primary btn-xl col-span-2" onclick={() => pick(1)}>
+				{#if loading === 1}
+					<div class="loading loading-md" transition:slide={{ axis: 'x' }}></div>
+				{/if}
+				{#if selectedAyat.length}
+					پذیرفتن یک آیه بیشتر
+				{:else}
+					پذیرفتن خواندن یک آیه
+				{/if}
+			</button>
+
+			{@render smallButton('پذیرفتن ۵ آیه متوالی', 5)}
+			{@render smallButton('پذیرفتن ۱۰ آیه متوالی', 10)}
+			{@render smallButton('پذیرفتن ۲۰ آیه متوالی', 20)}
+			{@render smallButton('پذیرفتن ۴۰ آیه متوالی', 40)}
+		</div>
 	</div>
 </div>

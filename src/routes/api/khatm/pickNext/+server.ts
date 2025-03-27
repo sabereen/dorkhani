b17eac65cf14e18a497d8461027f67c9b2720943
sabereen <mqtstbh@gmail.com>
2025-03-27 05:18:25 +0330

    fix: fix bug in start of pick

--- src/routes/api/khatm/pickNext/+server.ts
+++ src/routes/api/khatm/pickNext/+server.ts
@@ -24,24 +24,24 @@ export const POST: RequestHandler = async (event) => {
 		throw error(400, 'ورودی معتبر نیست')
 	}
 
-	body.count = Math.floor(body.count)
+	const count = Math.floor(body.count)
 
 	const result = await db.khatm.update({
 		where: {
 			id: body.khatmId,
 			sequential: true,
 			rangeType: 'ayah',
-			currentAyahIndex: { lt: COUNT_OF_AYAHS - body.count },
+			currentAyahIndex: { lt: COUNT_OF_AYAHS - count },
 		},
 		data: {
-			currentAyahIndex: { increment: body.count },
+			currentAyahIndex: { increment: count },
 		},
 	})
 
 	const text = await importText('hafs')
 
 	const ayat: SelectedAyah[] = []
-	for (let i = body.count - 1; i >= 0; i--) {
+	for (let i = count; i > 0; i--) {
 		const ayahIndex = result.currentAyahIndex - i
 		ayat.push({
 			index: ayahIndex,

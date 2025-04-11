90347e86aa664d2c72a3c96890babee3946077e3
sabereen <mqtstbh@gmail.com>
2025-04-11 08:17:35 +0330

    feat: add better browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import { svelteTesting } from '@testing-library/svelte/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import icons from 'unplugin-icons/vite'
 import UnoCSS from 'unocss/vite'
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), UnoCSS(), icons({ autoInstall: true, compiler: 'svelte' })],
+	plugins: [
+		sveltekit(),
+		UnoCSS(),
+		icons({ autoInstall: true, compiler: 'svelte' }),
+		legacy({
+			renderLegacyChunks: false,
+			renderModernChunks: true,
+			modernPolyfills: true,
+			modernTargets: ['edge>=79, firefox>=67, chrome>=64, safari>=12'],
+		}),
+	],
+
+	build: {
+		target: ['edge79', 'firefox67', 'chrome64', 'safari12'],
+	},
 
 	test: {
 		workspace: [

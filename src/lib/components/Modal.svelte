6d3a82823e6ee3f94d164561ad7e0c10d05fe755
sabereen <mqtstbh@gmail.com>
2025-04-12 07:03:44 +0330

    fix: fix modal for legacy browsers

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -3,18 +3,28 @@
 	import { fade, scale } from 'svelte/transition'
 
 	let { open = $bindable(false), children = null as null | Snippet } = $props()
+
+	function close() {
+		open = false
+	}
+
+	function handleKeyboard(event: KeyboardEvent) {
+		if (event.key === 'Escape') close()
+	}
 </script>
+
+<svelte:document onkeyup={handleKeyboard} />
 
 <input class="modal-toggle" type="checkbox" hidden checked={open} />
 
 {#if open}
-	<div class="modal" out:fade>
+	<div class="modal flex! items-center justify-center" out:fade>
 		<button
 			in:fade|global
 			type="button"
 			aria-label="close"
-			class="absolute inset-0 bg-white/20"
-			onclick={() => (open = false)}
+			class="absolute inset-0 size-full cursor-default bg-white/20"
+			onclick={close}
 		></button>
 		<div class="modal-box" transition:scale|global={{ start: 0.8, opacity: 0 }}>
 			{@render children?.()}

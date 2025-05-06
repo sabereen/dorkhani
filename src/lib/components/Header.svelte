dc6ae07ddf5983a5add09451c1b5870742e64fd7
sabereen <mqtstbh@gmail.com>
2025-05-06 04:51:47 +0330

    feat: add back to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte'
+	import IconBack from '~icons/ic/round-arrow-forward-ios'
 
 	type Props = {
 		title: string
@@ -9,10 +10,22 @@
 	}
 
 	const { title = '', link, end, start }: Props = $props()
+
+	function back() {
+		history.back()
+	}
 </script>
 
 <div class="navbar bg-base-300 shadow-sm">
-	<div class="navbar-start flex items-center">{@render start?.()}</div>
+	<div class="navbar-start flex items-center">
+		{#if start}
+			{@render start()}
+		{:else}
+			<button type="button" class="btn btn-circle !btn-ghost" aria-label="Back" onclick={back}>
+				<IconBack />
+			</button>
+		{/if}
+	</div>
 	<div class="navbar-center">
 		<h1 class="select-none text-xl font-black">
 			{#if link}

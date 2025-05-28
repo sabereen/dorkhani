6cb73a2c9025047dcaa10619c19dd3f14c318ab4
sabereen <mqtstbh@gmail.com>
2025-05-28 18:38:27 +0330

    feat: add web app manifest

---
+++ src/routes/manifest.json/+server.ts
@@ -0,0 +1,28 @@
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { base } from '$app/paths'
+
+export const GET: RequestHandler = () => {
+	return json({
+		name: 'ختم قرآن گروهی',
+		short_name: 'ختم قرآن گروهی',
+		theme_color: '#1b031c',
+		background_color: '#1b031c',
+		display: 'standalone',
+		orientation: 'portrait',
+		scope: base + '/',
+		start_url: base + '/',
+		icons: [
+			{
+				src: '/icon-192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				src: '/icon-512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+	})
+}

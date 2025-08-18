c66537ebc4f21665dad37d7f3dbbe9141ed606b2
mm.m2matin <mm.m2matin@gmail.com>
2025-08-18 15:41:41 +0330

    adding percentage to setting page

--- src/routes/admin/showcase/+page.svelte
+++ src/routes/admin/showcase/+page.svelte
@@ -81,21 +81,16 @@
 		</div>
 		<p class="whitespace-pre-wrap text-xs opacity-85">{khatm.description}</p>
 	</div>
-	<div class="grid-gap-1 ms-auto grid shrink-0 grid-cols-3 items-center">
-        <a
-            class="btn btn-xs btn-square !btn-ghost p-0 grid grid-cols-3 items-center"
-            href={khatm.link}
-            target="_blank"
-            style="background-color: {khatm.percent === 100 ? '#10B981' : '#6c757d'};"
+	<div class="flex items-center gap-2 shrink-0">
+        <span
+            class="badge text-xs px-2 py-1 rounded"
+            class:bg-green-500/20={khatm.percent === 100}
+            class:opacity-75={khatm.percent !== 100}
         >
-            <span
-                class="badge text-xs text-white"
-                class:text-green-500={khatm.percent === 100}
-                class:opacity-75={khatm.percent !== 100}
-            >
-                {khatm.percent.toLocaleString('fa')}%
-            </span>
-            <IconLink class="size-5 ml-2" />
+            {khatm.percent.toLocaleString('fa')}%
+        </span>
+        <a class="btn btn-xs btn-primary btn-square !btn-ghost p-0" href={khatm.link} target="_blank">
+            <IconLink class="size-5" />
         </a>
         <button
             class="btn btn-xs btn-primary btn-square !btn-ghost p-0"

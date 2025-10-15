33d31b3608c1613bbbb9ccb9d2fc9a3ae6c1bed5
sabereen <mqtstbh@gmail.com>
2025-10-15 07:34:09 +0330

    refactor: move ayahInfo extraction to its own service

---
+++ src/lib/server/service/quran.ts
@@ -0,0 +1,42 @@
+import translationAnsarian from '@ghoran/translation/json/fa/tanzil-ansarian.json'
+import translationMakarem from '@ghoran/translation/json/fa/tanzil-makarem.json'
+import translationGharaati from '@ghoran/translation/json/fa/tanzil-gharaati.json'
+
+import quranTextQPC1 from '@ghoran/text/json/quran-text-qpc-v1.json'
+import quranTextQPC2 from '@ghoran/text/json/quran-text-qpc-v2.json'
+import quranTextHafs from '@ghoran/text/json/quran-text-hafs.json'
+
+export type AyahInfo = {
+	index: number
+	textQPC1: string
+	textQPC2: string
+	textHafs: string
+	translation: string
+}
+
+const translationMap = {
+	ansarian: translationAnsarian,
+	makarem: translationMakarem,
+	gharaati: translationGharaati,
+}
+
+export type Translation = keyof typeof translationMap
+
+export function getAyahInfo(ayahIndex: number, translation: Translation) {
+	const translationStrings = translationMap[translation] || translationAnsarian
+	return {
+		index: ayahIndex,
+		textQPC1: quranTextQPC1[ayahIndex],
+		textQPC2: quranTextQPC2[ayahIndex],
+		textHafs: quranTextHafs[ayahIndex],
+		translation: translationStrings[ayahIndex],
+	}
+}
+
+export function getAyahInfoRange(range: { start: number; end: number }, translation: Translation) {
+	const result: AyahInfo[] = []
+	for (let i = range.start; i <= range.end; i++) {
+		result.push(getAyahInfo(i, translation))
+	}
+	return result
+}
